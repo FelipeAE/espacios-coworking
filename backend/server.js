3f81f638bc3ace5102a901ab66da1803f9a0fbdd
Rodrigo <rodrigo.gutierrezgh@gmail.com>
2024-10-19 21:37:55 -0300

    backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: ''
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
